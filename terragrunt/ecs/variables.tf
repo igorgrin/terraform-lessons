599bddfa73c97cfb55e347983ba07f23f33606c9
Igor Grinkin <igor.grinkin@newfront.com>
2023-09-13 11:03:41 -0700

    adding a loop for multiple services

--- terragrunt/ecs/variables.tf
+++ terragrunt/ecs/variables.tf
@@ -13,6 +13,28 @@ variable "container_port" {
   type = number
 }
 variable "cluster_name" {}
+variable "task_name" {}
 variable "service_name" {}
 variable "service_scaled" {}
-variable "task_name" {}
+
+# A list of multiple services
+variable "services" {
+  type = list(object({
+    name            = string
+    task_definition = string
+  }))
+  default = [
+    {
+      name            = "service-a"
+      task_definition = aws_ecs_task_definition.my_task_a.arn
+    },
+    {
+      name            = "service-b"
+      task_definition = aws_ecs_task_definition.my_task_b.arn
+    },
+    {
+      name            = "service-c"
+      task_definition = aws_ecs_task_definition.my_task_c.arn
+    }
+  ]
+}
