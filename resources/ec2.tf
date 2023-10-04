47d4c52f9421f035fec5ef2ec0c377a0c2ad4ea4
Igor Grinkin <igor.grinkin@newfront.com>
2023-10-04 15:01:45 -0700

    adding volume

--- resources/ec2.tf
+++ resources/ec2.tf
@@ -18,6 +18,21 @@ module "ec2_instance" {
   }
 }
 
+resource "aws_volume_attachment" "this" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.this.id
+  instance_id = module.ec2_instance.id
+}
+
+resource "aws_ebs_volume" "this" {
+  availability_zone = module.ec2_instance.availability_zone
+  size              = 1
+
+  tags = {
+    Name = "test"
+  }
+}
+
 # DNS name external
 resource "aws_route53_record" "ec2_instance_external" {
   zone_id = aws_route53_zone.main.id
